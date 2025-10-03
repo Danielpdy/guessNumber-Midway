1c6db41712bb86c125d64bcff967f0701f1d62ca
Daniel Pinzon <pinzond723@gmail.com>
2025-10-02 23:35:15 -0400

    Guessing number logic fixed and working but still the attemps counter is broken

--- src/App.jsx
+++ src/App.jsx
@@ -2,50 +2,60 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const randomNumber = (min, max) => Math.floor(Math.random()*(max-min+1)) + min;
   
   const [playerName, setPlayerName] = useState("");
-  const [greeting, setGreeting] = useState("");
   const [attemps, setAttemps] = useState(0);
   const [showAttemps, setShowAttemps] = useState(null);
   const [mode, setMode ] = useState("name");
   const [input, setInput] = useState();
-  const [guess, setGuess] = useState();
+  const [guess, setGuess] = useState(null);
   const [winnerNumber, setWinnerNumber] = useState(() => randomNumber(1, 100));
 
-  const randomNumber = (min, max) => Math.floor(Math.random()*(max-min+1)) + min;
 
   const handleClick = () => {
-    setGreeting(`Welcome ${playerName}`);
+
+    if (mode === "name") {
+    setPlayerName(`Welcome ${input.trim()}`);
     setShowAttemps(`Attempts: ${attemps}`);
     setMode("guess");
     setInput("");
+    } else {
+      const guess = Number(input);
+      setGuess(guess);
+      setShowAttemps(`Attemps: ${attemps + 1}`);
+      setInput("");
+
+    }
   }
 
   function guessNumber () {
-    if (guess > winnerNumber){
-      
-    }
+    if (guess === null) return <p></p>;
+    if (guess === winnerNumber) return <p>You succesfully guessed the number</p>;
+    return guess > winnerNumber ? <p>Your number is a little to high!</p>
+     : <p>Your number is a little too low!</p>;
   }
+
+
 
   return (
     <>
       <div className='mainContainer'>
         <section className='contentContainer'>
           <h1>Guess the number</h1>
-          <p>{greeting}</p>
+          <p>{playerName}</p>
           <p>{showAttemps}</p>
 
-          <input type="text" placeholder={mode === "name" ? "Enter your name"
-            : "Enter your guess"} 
+          <input className='input' type="text" placeholder={mode === "name" ? "Enter your name"
+          : "Enter your guess"} 
           value={input}
-          onChange={(e) => setPlayerName(e.target.value) 
-          && setGuess(e.target.value)}/>
+          onChange={(e) => setInput(e.target.value)}/>
 
           <button onClick={handleClick}>
             {mode === "name" ? "Add name" : "Add guess"}
           </button>
 
-          {mode === "guess" ? <p>text2</p> : <p>text</p>}
+           {guessNumber()} 
         </section>
       </div>
     </>
